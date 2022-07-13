f587fab848a7153b8a22368bb446d12db2d951ee
Artem Abramov <Artem_Abramov1@epam.com>
2022-07-13 22:02:52 +0300

    custom cookie consent banner

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,18 @@ import 'prismjs/themes/prism-tomorrow.css';
 import Script from 'next/script';
 
 function MyApp({ Component, pageProps }) {
+  const allowAllHandler = (e) => {
+    e.preventDefault();
+    OneTrust.AllowAll();
+  };
+  const rejectAllHandler = (e) => {
+    e.preventDefault();
+    OneTrust.RejectAll();
+  };
+  const settingsHandler = (e) => {
+    e.preventDefault();
+    OneTrust.ToggleInfoDisplay();
+  };
   return (
     <>
       <link
@@ -25,6 +37,51 @@ function MyApp({ Component, pageProps }) {
         }}
       />
       <span className="theme-bejamas" />
+      <div className="bg-indigo-600 w-full">
+        <div className="max-w-7xl mx-auto py-3 px-3 sm:px-6 lg:px-8">
+          <div className="flex items-center justify-between flex-wrap">
+            <div className="w-0 flex-1 flex items-center">
+              <p className="ml-3 font-medium text-white truncate">
+                <span className="hidden md:inline">
+                  By clicking “Accept All Cookies”, you agree to the storing of
+                  cookies.
+                </span>
+              </p>
+            </div>
+            <div className="flex space-x-4 order-3 mt-2 flex-shrink-0 w-full sm:order-2 sm:mt-0 sm:w-auto">
+              <a
+                href="#"
+                className="flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-indigo-600 bg-white hover:bg-indigo-50"
+                onClick={settingsHandler}
+              >
+                Cookie Settings
+              </a>
+              <a
+                href="#"
+                className="flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-900 hover:bg-indigo-700"
+                onClick={rejectAllHandler}
+              >
+                Reject All
+              </a>
+              <a
+                href="#"
+                className="flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-indigo-700 bg-indigo-100 hover:bg-indigo-200"
+                onClick={allowAllHandler}
+              >
+                Accept All Cookies
+              </a>
+            </div>
+            <div className="order-2 flex-shrink-0 sm:order-3 sm:ml-3">
+              <button
+                type="button"
+                className="-mr-1 flex p-2 rounded-md hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-white sm:-mr-2"
+              >
+                <span className="sr-only">Dismiss</span>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
       <Component {...pageProps} />
     </>
   );
