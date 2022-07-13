6b17bbce66ecdaf9aa4053fbab84e1decbee8f47
Artem Abramov <Artem_Abramov1@epam.com>
2022-07-13 19:59:17 +0300

    add cookiepro script correct way

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,29 @@
 import '../styles/globals.css';
 import 'prismjs/themes/prism-tomorrow.css';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <link
+        rel="preload"
+        as="script"
+        href="https://cookie-cdn.cookiepro.com/scripttemplates/otSDKStub.js"
+      />
+      <Script
+        id="cookieProSDK"
+        src="https://cookie-cdn.cookiepro.com/scripttemplates/otSDKStub.js"
+        type="text/javascript"
+        charset="UTF-8"
+        data-domain-script="6633f9f2-4d02-46b4-9ac6-8b26fadb8e30-test"
+      ></Script>
+      <Script
+        id="cookieProScript"
+        type="text/javascript"
+        dangerouslySetInnerHTML={{
+          __html: `function OptanonWrapper() {}`,
+        }}
+      />
       <span className="theme-bejamas" />
       <Component {...pageProps} />
     </>
--- pages/_document.js
+++ pages/_document.js
@@ -1,27 +1,11 @@
 import React from 'react';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
-import Script from 'next/script';
 
 class MyDocument extends Document {
   render() {
     return (
       <Html lang="en" className="theme-compiled">
-        <Head>
-          <Script
-            id="cookieProSDK"
-            src="https://cookie-cdn.cookiepro.com/scripttemplates/otSDKStub.js"
-            type="text/javascript"
-            charset="UTF-8"
-            data-domain-script="6633f9f2-4d02-46b4-9ac6-8b26fadb8e30-test"
-          ></Script>
-          <Script
-            id="cookieProScript"
-            type="text/javascript"
-            dangerouslySetInnerHTML={{
-              __html: `function OptanonWrapper() {}`,
-            }}
-          />
-        </Head>
+        <Head />
         <body
           className={`antialiased text-lg bg-white dark:bg-gray-900 dark:text-white leading-base`}
         >
