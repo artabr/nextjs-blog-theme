ec9f25597f5cbd8ed604e8eea8a7399a1bf99997
Artem Abramov <Artem_Abramov1@epam.com>
2022-07-14 19:12:54 +0300

    production script

--- pages/_app.js
+++ pages/_app.js
@@ -24,10 +24,10 @@ function MyApp({ Component, pageProps }) {
       />
       <Script
         id="cookieProSDK"
-        src="https://cookie-cdn.cookiepro.com/consent/6633f9f2-4d02-46b4-9ac6-8b26fadb8e30-test/otSDKStub.js"
+        src="https://cookie-cdn.cookiepro.com/consent/6633f9f2-4d02-46b4-9ac6-8b26fadb8e30/otSDKStub.js"
         type="text/javascript"
         charset="UTF-8"
-        data-domain-script="6633f9f2-4d02-46b4-9ac6-8b26fadb8e30-test"
+        data-domain-script="6633f9f2-4d02-46b4-9ac6-8b26fadb8e30"
       ></Script>
       <Script
         id="cookieProScript"
